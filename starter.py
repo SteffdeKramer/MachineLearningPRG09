0bc97ca0cf93e8bdfd7f54bf19d294640a107c37
Stef <stefdekramer3@gmail.com>
2019-02-26 19:09:01 +0100

    almost done

--- starter.py
+++ starter.py
@@ -12,8 +12,14 @@ Benodigde libraries:
 """
 from machinelearningdata import Machine_Learning_Data
 import numpy as np
+import sklearn
 import matplotlib.pyplot as plt
+from sklearn import tree
 from sklearn.metrics import accuracy_score
+from sklearn import datasets
+
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
 
 from sklearn.cluster import KMeans
 
@@ -61,7 +67,7 @@ plt.show()
 # TODO: ontdek de clusters mbv kmeans en teken een plot met kleurtjes
 
 
-kmeans = KMeans(n_clusters=2)
+kmeans = KMeans(n_clusters=3)
 kmeans = kmeans.fit(X)
 labels = kmeans.predict(X)
 centroids = kmeans.cluster_centers_
@@ -89,20 +95,31 @@ b = X[...,1]
 
 # teken de punten
 for i in range(len(a)):
-    plt.plot(a[i], b[i], 'k.') # k = zwart
+
+    if Y[i] == 0:
+        plt.plot(a[i], b[i], 'k.')
+    else:
+        plt.plot(a[i], b[i], 'r.')
 
 plt.axis([min(a), max(a), min(b), max(b)])
-
 plt.show()
 
-
-
-print(X)
-print(Y)
+# vergelijk y waarde met predict y waarde
 
 
 # TODO: leer de classificaties, en kijk hoe goed je dat gedaan hebt door je voorspelling te vergelijken
 # TODO: met de werkelijke waarden
+
+clf = LogisticRegression(max_iter=5000).fit(X, Y)
+Y_pred = clf.predict(X[:, :])
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
+Y_d_pred = clf.predict(X[:, :])
+predict_1 = accuracy_score(Y, Y_pred)
+predict_2 = accuracy_score(Y, Y_d_pred)
+
+print(predict_1)
+print(predict_2)
 
 
 # haal data op om te testen
@@ -112,7 +129,20 @@ X_test = extract_from_json_as_np_array("x", classification_test)
 
 # TODO: voorspel de Y-waarden
 
+x = X_test[...,0]
+y = X_test[...,1]
 
+# teken de punten
+for i in range(len(x)):
+        plt.plot(x[i], y[i], 'b.')
+
+plt.axis([min(x), max(x), min(y), max(y)])
+plt.show()
+
+# tryal
+
+clf = LogisticRegression(max_iter=5000).fit(X, Y)
+Y_pred = clf.predict(X[:, :])
 
 
 Z = np.zeros(100) # dit is een gok dat alles 0 is... kan je zelf voorspellen hoeveel procent er goed voorspeld is?
